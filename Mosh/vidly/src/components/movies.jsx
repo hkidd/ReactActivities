c9b009f039a677af6e019c20f32083aee82be16d
hkidd <harrisonakidd@gmail.com>
2021-10-05 14:11:47 -0500

    extracted method for getting page data

--- Mosh/vidly/src/components/movies.jsx
+++ Mosh/vidly/src/components/movies.jsx
@@ -5,7 +5,7 @@ import { getMovies } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
 import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
-import _ from 'lodash';
+import _ from "lodash";
 
 class Movies extends Component {
   state = {
@@ -13,13 +13,13 @@ class Movies extends Component {
     genres: [],
     currentPage: 1,
     pageSize: 4,
-    sortColumn: { path: 'title', order: 'asc' }
+    sortColumn: { path: "title", order: "asc" },
   };
 
   componentDidMount() {
-    const genres = [{ _id: "", name: 'All Genres'}, ...getGenres()];
+    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
 
-    this.setState({ movies: getMovies(), genres })
+    this.setState({ movies: getMovies(), genres });
   }
 
   handleDelete = (movie) => {
@@ -43,48 +43,67 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
-  handleSort = sortColumn => {
+  handleSort = (sortColumn) => {
     this.setState({ sortColumn });
+  };
+
+  getPageData = () => {
+    const {
+      pageSize,
+      currentPage,
+      selectedGenre,
+      movies: allMovies,
+      sortColumn,
+    } = this.state;
+
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+        : allMovies;
+
+    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+    const movies = paginate(sorted, currentPage, pageSize);
+
+    return { totalCount: filtered.length, data: movies };
   };
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, selectedGenre, movies: allMovies, sortColumn } = this.state;
+    const {
+      pageSize,
+      currentPage,
+      sortColumn,
+    } = this.state;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
-    const filtered = selectedGenre && selectedGenre._id 
-    ? allMovies.filter(m => m.genre._id === selectedGenre._id) 
-    : allMovies;
-
-    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order])
-
-    const movies = paginate(sorted, currentPage, pageSize);
+    const { totalCount, data: movies } = this.getPageData();
 
     return (
       <div className="row">
         <div className="col-3">
-          <ListGroup 
-          items={this.state.genres}
-          selectedItem={this.state.selectedGenre}
-          onItemSelect={this.handleGenreSelect} 
+          <ListGroup
+            items={this.state.genres}
+            selectedItem={this.state.selectedGenre}
+            onItemSelect={this.handleGenreSelect}
           />
         </div>
         <div className="col">
-        <p>Showing {filtered.length} movies in the database.</p>
-        <MoviesTable 
-          movies={movies}
-          sortColumn={sortColumn} 
-          onLike={this.handleLike} 
-          onDelete={this.handleDelete}
-          onSort={this.handleSort} 
-        />
-        <Pagination
-          itemsCount={filtered.length}
-          pageSize={pageSize}
-          onPageChange={this.handlePageChange}
-          currentPage={currentPage}
-        />
+          <p>Showing {totalCount} movies in the database.</p>
+          <MoviesTable
+            movies={movies}
+            sortColumn={sortColumn}
+            onLike={this.handleLike}
+            onDelete={this.handleDelete}
+            onSort={this.handleSort}
+          />
+          <Pagination
+            itemsCount={totalCount}
+            pageSize={pageSize}
+            onPageChange={this.handlePageChange}
+            currentPage={currentPage}
+          />
         </div>
       </div>
     );
