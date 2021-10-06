81520dcf2f96ed4d1f252c368eb5be4b7f9026d5
hkidd <harrisonakidd@gmail.com>
2021-10-06 11:14:59 -0500

    Joi schema created for validation

--- Mosh/vidly/src/components/loginForm.jsx
+++ Mosh/vidly/src/components/loginForm.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Joi from 'joi-browser'
+import Joi from "joi-browser";
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -9,25 +9,26 @@ class LoginForm extends Component {
   };
 
   schema = {
-      username: Joi.string().required(),
-      password: Joi.string().required()
-  }
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
 
   validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
 
-    const result = Joi.validate(this.state.account, this.schema, { abortEarly: false });
-    console.log(result);
+    if (!error) return null;
 
     const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
 
-    const { account } = this.state;
-    if (account.username.trim() === "")
-      errors.username = "Username is required.";
-
-    if (account.password.trim() === "")
-      errors.password = "Password is required.";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   };
 
   handleSubmit = (e) => {
@@ -41,15 +42,6 @@ class LoginForm extends Component {
 
     // Call the server
     console.log("Submitted");
-  };
-
-  validateProperty = ({ name, value }) => {
-    if (name === 'username') {
-        if (value.trim() === "") return 'Username is required.';
-    }
-    if (name === 'password') {
-        if (value.trim() === "") return 'Password is required.';
-    }
   };
 
   handleChange = ({ currentTarget: input }) => {
