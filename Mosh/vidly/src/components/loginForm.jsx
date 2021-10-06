a561b7e425574b12b82d0b9928d32b915723c6c3
hkidd <harrisonakidd@gmail.com>
2021-10-06 11:03:48 -0500

    setting up Joi for validation

--- Mosh/vidly/src/components/loginForm.jsx
+++ Mosh/vidly/src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from 'joi-browser'
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -7,7 +8,16 @@ class LoginForm extends Component {
     errors: {},
   };
 
+  schema = {
+      username: Joi.string().required(),
+      password: Joi.string().required()
+  }
+
   validate = () => {
+
+    const result = Joi.validate(this.state.account, this.schema, { abortEarly: false });
+    console.log(result);
+
     const errors = {};
 
     const { account } = this.state;
@@ -33,10 +43,25 @@ class LoginForm extends Component {
     console.log("Submitted");
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === 'username') {
+        if (value.trim() === "") return 'Username is required.';
+    }
+    if (name === 'password') {
+        if (value.trim() === "") return 'Password is required.';
+    }
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+
+    this.setState({ account, errors });
   };
 
   render() {
