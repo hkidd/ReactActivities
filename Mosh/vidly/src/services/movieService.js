9236615ec63299fec6b721f08ccf171834d20b09
hkidd <harrisonakidd@gmail.com>
2021-10-07 11:51:35 -0500

    retrieving movies and genres from the backend, + refactoring

--- Mosh/vidly/src/services/movieService.js
+++ Mosh/vidly/src/services/movieService.js
@@ -1,11 +1,18 @@
 import http from "./httpServices";
-import {apiUrl} from '../config.json';
+import { apiUrl } from "../config.json";
+import { getGenres } from "./genreService";
 
 const apiEndpoint = apiUrl + "/movies";
 
 export function getMovies() {
   return http.get(apiEndpoint);
 }
+
+export function getMovie(movieId) {
+  return http.get(apiEndpoint + "/" + movieId);
+}
+
+export function saveMovie(movie) {}
 
 export function deleteMovie(movieId) {
   return http.delete(apiEndpoint + "/" + movieId);
